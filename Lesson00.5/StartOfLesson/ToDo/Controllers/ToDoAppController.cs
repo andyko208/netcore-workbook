661ee144850605fc9d7fd9bf073265365ac3a5b4
Kyung Myung Ko <peanut61313@gmail.com>
2019-01-14 17:40:07 -0600

    Lesson.5

--- Lesson00.5/StartOfLesson/ToDo/Controllers/ToDoAppController.cs
+++ Lesson00.5/StartOfLesson/ToDo/Controllers/ToDoAppController.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
 
-namespace ToDo.Controllers
+namespace ToDoApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ToDoAppController : ControllerBase
+    public class ToDoController : Controller
     {
+        public List<Models.ToDo> list = new List<Models.ToDo>
+            {
+                new Models.ToDo {Id = 1, Description = "Do Laundry", Status = new Status() { Id = 1, Value = "Pending" }},
+                new Models.ToDo {Id = 2, Description = "Go To HEB", Status = new Status() { Id = 2, Value = "Pending" }},
+                new Models.ToDo {Id = 3, Description = "Relax", Status = new Status() { Id = 3, Value = "Complete"}}
+            };
+
+        public IActionResult Index()
+        {
+            return View(list);
+        }
+
     }
 }
---
+++ Lesson00.5/StartOfLesson/ToDo/Models/ToDo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        [UIHint("Status")]
+        public Status Status { get; set; }
+    }
+}
